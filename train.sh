a94a77fceaf8af0aa3bc3fc16cc103c6bbb7e2df
Nafees Hassan <nafeeshassan16@gmail.com>
2024-08-20 16:20:19 +0500

    Made Thanos see the GPU

--- train.sh
+++ train.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-THEANO_FLAGS='floatX=float32' python main.py \
---dataset "$1/all" \
---save_result "$1/log" \
---n_epoch 5 \
---batch_size 10 \
---max_length 50 \
---hidden_size 200 \
---word_embedding_size 200 \
---session_hidden_size 50 \
---session_input_size 50 \
---model_name DUA.bin \
---val_frequency 10 \
---max_turn 10 \
+THEANO_FLAGS='floatX=float32,device=cuda' python main.py \
+	--dataset "$1/all" \
+	--save_result "$1/log" \
+	--n_epoch 5 \
+	--batch_size 10 \
+	--max_length 50 \
+	--hidden_size 200 \
+	--word_embedding_size 200 \
+	--session_hidden_size 50 \
+	--session_input_size 50 \
+	--model_name DUA.bin \
+	--val_frequency 10 \
+	--max_turn 10
